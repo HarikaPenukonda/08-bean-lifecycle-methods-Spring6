d4734e5f0299cfdeae2f550a7a17025fafeadd89
harika penukonda <penukondaharika@gmail.com>
2023-05-23 16:59:04 -0500

    initial commit

---
+++ src/main/java/com/luv2code/springcoredemo/common/CricketCoach.java
@@ -0,0 +1,18 @@
+package com.luv2code.springcoredemo.common;
+
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+@Component // This annotation marks the class as spring bean, makes it available for dependency injection
+public class CricketCoach implements Coach{
+
+    public CricketCoach(){
+        System.out.println("In Constructor: " + getClass().getSimpleName());
+    }
+
+    @Override
+    public String getDailyWorkout() {
+        return "Practice fast bowling for 15mins !!!";
+    }
+}
